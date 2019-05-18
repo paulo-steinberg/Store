f4dbfdddae6d1b1b10466c4d7f6d35b4d63596c2
Paulo Steinberg <pvcsteinberg@hotmail.com>
2019-05-18 13:43:27 -0300

    Creating Entities and Enums

---
+++ Domain/StoreContext/Entities/Delivery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.StoreContext.Enums;
+
+namespace Domain.StoreContext.Entities
+{
+    public class Delivery
+    {
+        public Delivery(DateTime estimateDeliveryDate)
+        {
+            CreatedAt = DateTime.Now;
+            DeliveryDate = estimateDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+
+        public DateTime CreatedAt { get; private set; }
+        public DateTime DeliveryDate { get; private set; }
+        public EDeliveryStatus Status { get; private set; }
+
+    }
+}
---
+++ Domain/StoreContext/Entities/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.StoreContext.Enums;
+
+namespace Domain.StoreContext.Entities
+{
+    public class Order
+    {
+        public Order(Customer customer)
+        {
+            Customer = customer;
+            Number = Guid.NewGuid().ToString().Replace("-","").Substring(0,8).ToUpper();
+            CreatedAtDate = DateTime.Now;
+            Status = EOrderStatus.Created;
+            Items = new List<OrderItem>();
+            Deliveries = new List<Delivery>();
+
+        }
+
+        public Customer Customer { get; private set; }
+        public string Number { get; private set; }
+        public DateTime CreatedAtDate { get; private set; }
+        public  EOrderStatus Status { get; private set; }
+        public IReadOnlyCollection<OrderItem> Items { get; private set; }
+        public IReadOnlyCollection<Delivery> Deliveries { get; private set; }
+
+        public void PlaceAnOrder()
+        {
+
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            //Valida Item
+            //Adiciona ao pedido
+        }
+    }
+}
