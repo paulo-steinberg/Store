dcf2c55928eaddd17ba300627421be3b4d9af035
Paulo Steinberg <pvcsteinberg@hotmail.com>
2019-05-26 17:57:52 -0300

    Created Commands

---
+++ Domain/StoreContext/Commands/Customer/Inputs/CreateCustomerCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.StoreContext.Commands.Customer.Inputs
+{
+    public class CreateCustomerCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ Domain/StoreContext/Commands/Order/Inputs/CreateOrderCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain.StoreContext.Commands.Order.Inputs
+{
+    public class CreateOrderCommand
+    {
+        public Guid Cusomer { get; set; }
+        public IList<OrderItemCommand> OrderItems { get; set; }
+
+    }
+
+    public class OrderItemCommand
+    {
+        public Guid Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
