8091dbe06080fcaad62b98a89a2567034c056512
Paulo Steinberg <pvcsteinberg@hotmail.com>
2019-06-09 21:11:17 -0300

    Updating database before get to the API part.

--- Tests/Mocks/CustomerRepositoryMock.cs
+++ Tests/Mocks/CustomerRepositoryMock.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.StoreContext.Entities;
+using Domain.StoreContext.Queries;
 using Domain.StoreContext.Repositories;
 
 namespace Tests.Mocks
@@ -13,6 +14,11 @@ namespace Tests.Mocks
         public bool CheckEmail(string email)
         {
             return false;
+        }
+
+        public CustomerOrdersCount GetCustomerOrdersCount(string document)
+        {
+            return new CustomerOrdersCount() { };
         }
 
         public void Save(Customer customer)
