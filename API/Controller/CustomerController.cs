8de08cf89b385892af405112ecbb7b763b795327
Paulo Steinberg <pvcsteinberg@hotmail.com>
2019-06-12 17:40:17 -0300

    Upgrade .net core version to 2.1

---
+++ API/Controller/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.StoreContext.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controller
+{
+    public class CustomerController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}")]
+        public Customer GetById(Guid id)
+        {
+            return null;
+        }
+
+        [HttpGet]
+        [Route("customers/{id}/orders")]
+        public List<Order> GetOrders(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody]Customer customer)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public Customer Delete()
+        {
+            return null;
+        }
+
+    }
+}
